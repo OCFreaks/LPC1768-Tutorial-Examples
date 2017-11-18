8330f505f1d3490cb2d68a8e14ef30e86748a888
Umang Gajera <26960740+UmangGajera@users.noreply.github.com>
2017-11-18 12:31:13 +0530

    Fixed bug

--- ADC/Example_1/main.cpp
+++ ADC/Example_1/main.cpp
@@ -27,6 +27,7 @@ int main(void)
 	LPC_PINCON->PINSEL1 |= (1<<14); //select AD0.0 for P0.23
 	LPC_ADC->ADCR =  ADCR_SETUP_SCM | SEL_AD0_0;
 	int result = 0;
+	float volts = 0;
 	
 	printf("OCFreaks.com LPC176x ADC Tutorial Example 1.\nSoftware Controlled ADC Mode on AD0.0 Channel.\n");
 	
@@ -37,10 +38,13 @@ int main(void)
 		while((LPC_ADC->ADDR0 & ADC_DONE) == 0); //Wait untill conversion is finished
 		
 		result = (LPC_ADC->ADDR0>>4) & 0xFFF; //12 bit Mask to extract result
-		printf("AD0.0 = %dmV\n" , (int)( result*VREF ));
+		
+		volts = (result*VREF)/4096.0; //Convert result to Voltage
+		
+		printf("AD0.0 = %dmV\n" , (int)(volts*1000)); //Display milli-volts
+		
 		delayMS(500); //Slowing down Updates to 2 Updates per second
 	}
 	
 	//return 0; //This won't execute
 }
-
